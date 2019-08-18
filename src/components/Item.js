a2842e1681af8daac4ebcbaec93ff038ece76ac1
Tom Becker <beckertomarch@gmail.com>
2019-08-18 19:24:21 +0300

    finish exercises

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,23 +3,26 @@ import { observer } from 'mobx-react'
 
 @observer
 class Item extends Component {
-    checkItem = () => {
-        //your code here
+    checkItem = (e) => {
+        this.props.store.checkItem(e.target.value)
       }
-      editItem = () => {
-        //your code here
-      }
-      deleteItem = () => {
-        //your code here
-      }
+    editItem = (e) => {
+        let itemName = e.target.getAttribute("data-id")
+        let newLocation = prompt("Enter a new location for the item:")
+        newLocation ? this.props.store.editItem(itemName, newLocation) : null
+    }
+    deleteItem = (e) => {
+        let itemName = e.target.getAttribute("data-id")
+        this.props.store.deleteItem(itemName)
+    }
     render() {
+      let item = this.props.item
         return (
-            <div className = "">
-      {/*   your code here
-            each item should be in an input checkbox
-            it should display the item name and location
-            it should have both an edit button and a delete button
-      */} 
+            <div className = {item.completed? "crossed" : null}>
+              <input type="checkbox" onClick={this.checkItem} value={item.name}/>
+              {item.name} {item.location}
+              <button className="editItem" data-id={item.name} onClick={this.editItem}>Edit</button>
+              <button className="deleteItem" data-id={item.name} onClick={this.deleteItem}>Delete</button>
             </div>)
     }
 }
--- src/stores/ShoppingList.js
+++ src/stores/ShoppingList.js
@@ -4,19 +4,23 @@ import { Item } from './Item'
 
 export class ShoppingList {
     // your code here
-    list = [];
-    length;
-    checkItem = () => {
-        // your code here
+    @observable list = [];
+    @observable length;
+    @action checkItem = (name) => {
+        let item = this.list.find( i => i.name === name )
+        item.completed = !item.completed
     }
-    addItem = () => {
-        // your code here
+    @action addItem = (name) => {
+        this.list.push(new Item(name))
     }
-    editItem = () => {
-        // your code here
+    @action editItem = (itemName, newLocation) => {
+        let item = this.list.find( i => i.name === itemName )
+        item.location = newLocation
     }
-    deleteItem = () => {
-        // your code here
+    @action deleteItem = (itemName) => {
+        let item = this.list.find( i => i.name === itemName )
+        let indexOfItem = this.list.indexOf(item)
+        this.list.splice(indexOfItem, 1)
     } 
 }
 
